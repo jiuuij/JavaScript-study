7127e015ad93e0ee51644731e4d15d45235f995f
jiu.kim <109861096+jiuuij@users.noreply.github.com>
2023-05-02 18:09:47 +0900

    Update rps.js
    $computer.style 함수로 빼기

--- jiu/RockPaperScissors/rps.js
+++ jiu/RockPaperScissors/rps.js
@@ -13,21 +13,22 @@ const imgX = {
     paper : '-275px', //보 
 };
 
-
-// if else if else 괄호안에 꺼는 함수로 빼서 사용 가능하다 - 리팩토링 할 때 해보기
-let coord = '0';
+let computerChoice = 'scissors';
+//setInterval이었다가 changeComputerHand로 함수를 만들어준 이유? 
+//데이터를 수정하는(computerChoice)와 화면을 수정하는 ($computer.style.background)로 나뉘어 있다.
+//데이터는 다른곳에서 자주 재사용되므로 데이터와 화면을 분리해서 프로그래밍 하는 것이 좋다.
 setInterval(() => {
-    if (coord === imgX.scissors) {// 가위면
-        coord = imgX.rock;
-        $computer.style.background  = `url(${IMG_URL}) ${imgX.rock} 0`;
-        $computer.style.backgroundSize = 'auto 200px'
-    } else if (coord === imgX.rock) {// 바위
-        coord = imgX.paper;
-        $computer.style.background  = `url(${IMG_URL}) ${imgX.paper} 0`;
-        $computer.style.backgroundSize = 'auto 200px'
-    } else if (coord === imgX.paper) { // 보 
-        coord = imgX.scissors;
-        $computer.style.background  = `url(${IMG_URL}) ${imgX.scissors} 0`;
-        $computer.style.backgroundSize = 'auto 200px'
+    if (computerChoice === 'scissors') {// 가위면
+        computerChoice = 'rock';
+    } else if (computerChoice === 'rock') {// 바위
+        computerChoice = 'paper';
+
+    } else { // 보 
+        computerChoice = 'scissors';
     }
+    //[]를 쓴 이유는 우린 변수의 값이 필요하기 때문이다. 
+    //computerChoice = 'scissors'에서 필요한 것은 'scissors'이고 그래야 imgX에서 지정한 값인 0을 가져오기 때문.
+    $computer.style.background  = `url(${IMG_URL}) ${imgX[computerChoice]} 0`;
+    $computer.style.backgroundSize = 'auto 200px'
 }, 80);
+
